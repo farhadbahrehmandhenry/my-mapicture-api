9e423ac40d4f0b4ed7009da096d1990a833a3435
Farhad Bahrehmand <43734894+farhadbahrehmandhenry@users.noreply.github.com>
2020-04-10 19:15:57 -0700

    add to heroku

--- server.js
+++ server.js
@@ -18,15 +18,9 @@ var database = knex({
   }
 });
 
-app.use(cors({
-  origin: 'localhost:3000'
-}));
+app.use(cors());
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.get('/', (req, res) => res.send('server is working'));
 app.post('/signin', (req, res) => {signin.handlaSignin(req, res, database, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, database, bcrypt)});
