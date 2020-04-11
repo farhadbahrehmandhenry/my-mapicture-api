f3fc80ff041b2faa79c26981eaf59ca8a80fb4e0
Farhad Bahrehmand <43734894+farhadbahrehmandhenry@users.noreply.github.com>
2020-04-10 19:02:00 -0700

    add to heroku

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ var database = knex({
   }
 });
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
 // app.use(cors());
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -25,9 +30,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/', (req, res) => res.send('server is working'));
-app.post('/signin', (req, res) => {signin.handlaSignin(req, res, database, bcrypt)});
-app.post('/register', (req, res) => {register.handleRegister(req, res, database, bcrypt)});
+app.get('/', cors(corsOptions), (req, res) => res.send('server is working'));
+app.post('/signin', cors(corsOptions), (req, res) => {signin.handlaSignin(req, res, database, bcrypt)});
+app.post('/register', cors(corsOptions), (req, res) => {register.handleRegister(req, res, database, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res)});
 // TODO get user liked images
 
